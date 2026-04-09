ec469a6bdd0238992bf03ea54b6b7ffc7d3d386f
Stones <ssonstones@gmail.com>
2026-04-09 22:01:04 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Shout Stones for what you need!",
+        name: "Shout Stones for what you need!:noFilter:",
         // Activity type number (0 = Playing).
         type: 3, 
       },
